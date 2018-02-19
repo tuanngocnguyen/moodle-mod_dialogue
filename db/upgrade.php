485d63710d6f5b81489536df91113ebf5b4954a4
Troy Williams <troy.williams@learningworks.co.nz>
2018-02-19 15:55:27 +1300

    New columns for conversations table.

--- db/upgrade.php
+++ db/upgrade.php
@@ -14,14 +14,63 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-function xmldb_dialogue_upgrade($oldversion=0) {
+function xmldb_dialogue_upgrade($oldversion = 0) {
     global $CFG, $DB, $OUTPUT;
 
     $dbman = $DB->get_manager(); // Loads ddl manager and xmldb classes.
 
     /**
-     * Moodle v2.8.0 release upgrade line.
+     * Moodle v3.4.0 release upgrade line.
      */
+
+    if ($oldversion < 2017111301) {
+        $table = new xmldb_table('dialogue_conversations');
+
+        // Conditionally rename field course to courseid.
+        $coursefield = new xmldb_field('course', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+        if (!$dbman->field_exists($table, $coursefield)) {
+            $dbman->rename_field($table, $coursefield, 'courseid');
+        }
+
+        // Conditionally add field ownerid.
+        $owneridfield = new xmldb_field('ownerid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        if (!$dbman->field_exists($table, $owneridfield)) {
+            $dbman->add_field($table, $owneridfield);
+        }
+
+        // Conditionally add field initiatorid.
+        $owneridfield = new xmldb_field('initiatorid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        if (!$dbman->field_exists($table, $owneridfield)) {
+            $dbman->add_field($table, $owneridfield);
+        }
+
+        // Conditionally add field recipientid.
+        $owneridfield = new xmldb_field('recipientid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        if (!$dbman->field_exists($table, $owneridfield)) {
+            $dbman->add_field($table, $owneridfield);
+        }
+
+        // Conditionally add field messagecount.
+        $messagecountfield = new xmldb_field('messagecount', XMLDB_TYPE_INTEGER, '3', null, XMLDB_NOTNULL, null, '0');
+        if (!$dbman->field_exists($table, $messagecountfield)) {
+            $dbman->add_field($table, $messagecountfield);
+        }
+
+        // Conditionally add field state.
+        $statefield = new xmldb_field('state', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0');
+        if (!$dbman->field_exists($table, $statefield)) {
+            $dbman->add_field($table, $statefield);
+        }
+
+        // Conditionally add field automated.
+        $automatedfield = new xmldb_field('hautomated', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0');
+        if (!$dbman->field_exists($table, $automatedfield)) {
+            $dbman->add_field($table, $automatedfield);
+        }
+
+        // Dialogue savepoint reached.
+        upgrade_mod_savepoint(true, 2017111301, 'dialogue');
+    }
 
     return true;
 }
--- version.php
+++ version.php
@@ -23,9 +23,10 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017111300;
+$plugin->version   = 2017111301;
 $plugin->requires  = 2017111300;        // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->component = 'mod_dialogue';    // Full name of the plugin (used for diagnostics)
-$plugin->release   = '3.4';             // Semantic version name
-$plugin->maturity  = MATURITY_STABLE;   // This version's maturity level.
+$plugin->release   = '3.4.0';           // Semantic version name
+$plugin->maturity  = MATURITY_ALPHA;   // This version's maturity level.
 $plugin->dependencies = array();
+
